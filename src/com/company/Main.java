641c59aef8936627be33147b5d713f547871f8dc
marti <martimasover@gmail.com>
2021-12-17 09:50:40 +0100

    Commit versió 1 desenvolupament

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Codi base");
+        System.out.println("Codi versio 1 desenvolupament");
 
     }
 }
